4e56a2e013a2bf2109b168a857d2836637868245
Will Studabaker <will.studabaker@gmail.com>
2020-10-23 15:53:59 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -19,15 +19,23 @@ all_y = ["Y", "Y", "Y"]
 
 def won?(board)
 count = 0
-until count >= 7 do
+  until count >= 7 do
   #use count as a selecter for index of WIN_COMBINATIONS
+   win_array = WIN_COMBINATIONS[count]
+   board_array = []
   #retrieve indexes from board that correspond to WIN_COMBINATIONS[i]
+    win_array.each do |index|
+      board_array << board[index]
+    end
   #if all x or y then game over (break)
-  #else increase count and try again
-  count += 1
-end
+  if board_array == all_x || board_array == all_y
+    return win_array
+  else
+    #else reset board_array and increase count and try again
+      board_array = []
+      count += 1
+  end
 
-
-
+  end
 
 end
