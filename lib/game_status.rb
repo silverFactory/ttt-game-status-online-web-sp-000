ef4be16a080f94057a34b1fdbfa30d6c0e2eccd5
Will Studabaker <will.studabaker@gmail.com>
2020-10-23 15:43:45 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -14,10 +14,18 @@ WIN_COMBINATIONS = [
   [0, 4, 8],
   [2, 4, 6]
 ]
-
-def won?(board)
 all_x = ["X", "X", "X"]
 all_y = ["Y", "Y", "Y"]
+
+def won?(board)
+count = 0
+until count >= 7 do
+  #use count as a selecter for index of WIN_COMBINATIONS
+  #retrieve indexes from board that correspond to WIN_COMBINATIONS[i]
+  #if all x or y then game over (break)
+  #else increase count and try again
+  count += 1
+end
 
 
 
